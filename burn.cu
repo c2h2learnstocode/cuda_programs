ded171c84dc2b26f0a372e197c43c02159ff126a
Yiling Cao <yiling.cao@gmail.com>
2018-03-09 01:11:58 +0800

    add gpu_burn.cu

---
+++ burn.cu
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void init(float *x, int s){
+    int i=0;
+    for(i=0; i<s; i++){
+        x[i]=1.0f * (float)i;
+    }
+}
+
+
+__global__
+void compute(float *x, float *y, int s){
+    int index =threadIdx.x;
+    int stride = blockDim.x;
+
+    int i=0;
+    for(i=index; i<s; i+=stride){
+      y[i]= x[i]*x[i]; 
+    }
+}
+
+int main(){
+    int N = 1<<27;
+    
+    float *x;// = malloc(sizeof(float)*N);
+    float *y;// = malloc(sizeof(float)*N);
+    
+    cudaMallocManaged(&x, sizeof(float)*N);
+    cudaMallocManaged(&y, sizeof(float)*N);
+
+    init(x, N);
+    init(y, N);
+    int i=0;
+    while(1){
+        compute<<<2,1024>>>(x, y, N);
+    }
+    cudaDeviceSynchronize();
+
+    for(i=0; i<N; i++){
+//        printf("%d %f %f\n", i, x[i], y[i]);
+
+    }
+}
